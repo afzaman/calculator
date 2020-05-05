e29c40e8f18b045a6cab53f9b6b763f1d436e126
afzaman <afzaman2003@gmail.com>
2020-05-05 09:15:10 -0400

    Prevent multiple decimals and final visuals changes

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 document.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
         operate();
-    } if (event.keyCode >= 96 && event.keyCode <= 111 
+    } if (event.keyCode >= 96 && event.keyCode <= 109 
         || event.keyCode >= 48 && event.keyCode <= 57 
         || event.keyCode === 189
         || event.keyCode === 187
+        || event.keyCode === 111
         || event.keyCode === 57){
             document.getElementById("display").textContent += event.key;
     } if (event.keyCode === 8 || event.keyCode === 46){
         deleteButton();
+    } if (event.keyCode === 110 || event.keyCode === 190){
+        decimalButton(".");
     };
+    ;
 });
 function operate(){
     var equationCompleted = document.getElementById("display").textContent;
@@ -21,6 +25,13 @@ function buttonPress(displayText){
 }
 function clearButton(){
     document.getElementById("display").textContent = "";
+}
+function decimalButton(displayText) {
+    var equationInProgress = document.getElementById("display").textContent;
+    if (equationInProgress.includes(".")){
+    } else {
+        document.getElementById("display").textContent += displayText;
+    }
 }
 function deleteButton(){
     var str = document.getElementById("display").textContent;
